4a4b6017bce87ecf41bf473b10b7a1e55faa416e
0308ehub <0308ehub@gmail.com>
2024-03-03 12:08:48 -0600

    Mar 3 12:08 PM

--- homepage.js
+++
@@ -1,7 +0,0 @@
-#there is like legit nothing here cause I still need to figure out alot of the java
-
-var settingsmenu = document.querySelector(".settings-menu");
-
-function settingsMenuToggle(){
-    settingsmenu.classList.toggle(".settings-menu-height");
-}
