6c81683750cdb80c1113a2c0faa6cb6e700142a0
A1uCard8 <79889432+A1uCard8@users.noreply.github.com>
2024-03-02 19:07:34 -0600

    Create homepage.js

---
+++ homepage.js
@@ -0,0 +1,7 @@
+#there is like legit nothing here cause I still need to figure out alot of the java
+
+var settingsmenu = document.querySelector(".settings-menu");
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle(".settings-menu-height");
+}
